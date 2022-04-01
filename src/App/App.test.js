9fdad80803cb76ad310c487d71bc1658fc55ca6c
jfangwang <qbs1864@gmail.com>
2022-04-01 01:23:55 -0400

    icons on sliding menu router have padding

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import App from './App';
+import { shallow } from 'enzyme';
+import { App } from './App';
+import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
 
-test('Renders without crashing', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.exists());
+describe('App Render Test', () => {
+  it('Renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true)
+  })
 })
