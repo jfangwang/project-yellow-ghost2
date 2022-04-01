35c763299b742ab7a66bdd1afc334ae6085649ad
jfangwang <qbs1864@gmail.com>
2022-04-01 17:43:36 -0400

    messages populated except for emoji

--- src/Screens/Messages/Message.js
+++ src/Screens/Messages/Message.js
@@ -3,8 +3,26 @@ import PropTypes from 'prop-types';
 import styles from './Message.module.css';
 import {connect} from 'react-redux';
 import {LinuxLogo} from 'phosphor-react';
+import TimeAgo from 'react-timeago';
+import enShort from 'react-timeago/lib/language-strings/en-short';
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
 
-
+const formatter = buildFormatter(enShort);
+const statusDict = {
+  'new-friend': 'New Friend!',
+  'new': 'New Snap',
+  'received': 'Received',
+  'sent': 'Sent',
+  'opened': 'Opened',
+  'pending': 'Pending',
+  'not-friends': 'Unfriended You',
+  'blocked': 'Blocked',
+};
+const emojiDict = {
+  'not-friends': '\u{1F494}',
+  'blocked': '\u{26D4}',
+  'pending': '\u{23F3}',
+};
 /**
  *
  *
@@ -12,12 +30,14 @@ import {LinuxLogo} from 'phosphor-react';
  * @return {*}
  */
 function Message(props) {
-  const {friend} = props;
-  console.log(friend);
+  const {friend, user} = props;
   return (
     <div className={styles.background}>
       <div className={styles.row}>
-        <div className={styles.row} style={{marginRight: '0.5rem'}}>
+        <div
+          className={styles.row}
+          style={{marginRight: '0.5rem', borderRadius: '1rem'}}
+        >
           <LinuxLogo size='4rem'/>
         </div>
         <div className={styles.col}>
@@ -28,13 +48,18 @@ function Message(props) {
             className={styles.row}
             style={{justifyContent: 'start'}}
           >
-            <h6>ICON</h6>
-            <h6>New Snap</h6>
+            {emojiDict[friend.status] ?
+              <p>{emojiDict[friend.status]}</p> :
+              <div className={styles.row}><p>EMOJI</p></div>
+            }
+            <h6>{statusDict[friend.status]}</h6>
             <div className={styles.separator}/>
-            <h6>TIME</h6>
+            <h6>
+              <TimeAgo date='3-24-2022, 1:50:23 AM' formatter={formatter}/>
+            </h6>
             <div className={styles.separator}/>
-            <h6>STREAK_NUM</h6>
-            <h6>EMOJI</h6>
+            <h6>{friend.streak}</h6>
+            <h6>{user.streakEmoji}</h6>
           </div>
         </div>
       </div>
@@ -49,12 +74,16 @@ Message.propTypes = {
   height: PropTypes.number,
   width: PropTypes.number,
   friend: PropTypes.object,
+  user: PropTypes.object,
 };
 
 Message.defaultProps = {
   height: window.innerHeight,
   width: window.innerWidth,
   friend: {
+    name: 'Error',
+  },
+  user: {
     name: 'Error',
   },
 };
